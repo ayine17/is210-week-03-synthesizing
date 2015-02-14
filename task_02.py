2dc27b4788bbdfa639684e3abe914f5f8eec4d7d
ayine Ernest <ayine17@gmail.com>
2015-02-14 05:41:21 +0000

    combination of .index(), slicing, and len()

--- task_02.py
+++ task_02.py
@@ -4,3 +4,13 @@
 
 
 import inquisition
+
+INDEXSPANISH = inquisition.SPANISH.index('Spanish')
+
+LEN_SPANISH = len('Spanish')
+
+LEFT_SLICE = inquisition.SPANISH[0:INDEXSPANISH]
+
+REGHT_SLICE = inquisition.SPANISH[INDEXSPANISH + LEN_SPANISH:]
+
+FLEMISH = LEFT_SLICE + 'Flemish' + REGHT_SLICE
