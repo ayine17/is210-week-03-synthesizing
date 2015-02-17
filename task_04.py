e62383b3d79d7a3c7c06a05b4faf3e7821d01cf2
ayine Ernest <ayine17@gmail.com>
2015-02-17 01:41:00 +0000

    change task_04.py

--- task_04.py
+++ task_04.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 """Provides variables for formatting."""
 
-NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
+NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1:06d}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
 
-RNUM = '{0:06}'.format(RNUM)
+#RNUM = '{RNUM:{fill}{width}}'.format(NRUM=123, fill='0', width=6)
+#RNUM= '{0:06}'.format(NRUM)
 
 EMAIL = NEWS.format(NTYPE, RNUM, friend=FNAME)
